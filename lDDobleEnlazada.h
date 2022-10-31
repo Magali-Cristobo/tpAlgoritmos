be3f91876491a03f87d4826c4c549da559e8778a
Magali-Cristobo <alumno21.cristobo.magali@ipm.edu.ar>
2022-10-31 20:35:00 -0300

    eliminamos comentarios innecesarios

--- lDDobleEnlazada.h
+++ lDDobleEnlazada.h
@@ -2,89 +2,6 @@
 #define LISTA_DINAMICA_DOBLEMENTE_ENLAZADA
 
 typedef int tDato;
-
-// typedef struct tNodo{
-//     tDato info;
-//     struct tNodo* sig;
-//     struct tNodo* ant;
-// }tNodo;
-
-// typedef struct tLista{
-//     tNodo* actual;
-//     tNodo* cab;
-// }tLista;
-
-
-
-// void lCrear(tLista* l){
-//     l->cab = NULL;
-//     l->actual = NULL;
-// }
-
-// int lVacia(tLista* l){
-//     return l->cab == NULL;
-// }
-
-// void crearNodo(tNodo* nodo,tDato x){ //esta bien asi?
-//     nodo=(tNodo*)malloc(sizeof(tNodo));
-//     nodo->info=x;
-//     nodo->sig = NULL;
-//     nodo->ant = NULL;
-// }
-
-// int lLlena(tLista *l){       //LISTO
-//     return 0;
-// }
-
-// void lPpio(tLista* l){           //LISTO
-//     l->actual = l->cab;
-//     printf("%i en lppio\n", (l->cab)->info);
-// }
-
-// int lFin(tLista *l){             //LISTO
-//     return l->actual == NULL;
-// }
-
-// void lInfo(tLista *l, tDato *x){ //probar
-//     *x = (l->actual)->info;
-//     printf("%i en linfo\n", (l->cab)->info);
-//     // printf("%i\n sdsdsd", *x);
-// }
-
-// void lModificar(tLista *l, tDato x){         //LISTO
-//     tNodo* aux = l->actual;
-//     aux->info = x;
-// }
-
-// void lSig(tLista *l){                //LISTO
-//     tNodo* aux = l->actual;
-//     l->actual = aux->sig;
-// }
-// void lInsertarPpio(tLista *l, tDato x){ // el problema deberia estar aca         //LISTO
-//     tNodo *nuevoNodo;
-//     crearNodo(nuevoNodo,x);
-//     nuevoNodo->info = x; // si ponemos esto aca fuciona pero no entiendo por que
-//     // if (l->cab != NULL){
-//     //     l->cab->ant = nuevoNodo;
-//     //     nuevoNodo->sig = l->cab;
-//     // }
-//     // l->cab = nuevoNodo;
-//     if (l->cab == NULL){
-//        l->cab = nuevoNodo;
-//     }
-//     else{
-//         nuevoNodo->sig = l->cab;
-//         (l->cab)->ant = nuevoNodo;
-//         l->cab = nuevoNodo;
-//     }
-    
-//     printf("%i en insertar\n", &(l->cab)->info);
-// }
-
-
-
-//=================================================================================================================
-//=================================================================================================================
 typedef struct nodo
 {
 	tDato info;
@@ -93,7 +10,6 @@ typedef struct nodo
 } nodoL;
 
 nodoL* crearNodo(tDato x);
-// Alias para lista
 
 typedef nodoL* lista;
 
@@ -280,5 +196,5 @@ void lBuscarOrdenado(tLista *l, tDato x, int *existe){// ver que onda con la cla
         }
     }
 }
-// insertarOrdenado, buscarOrdenado
+
 #endif
